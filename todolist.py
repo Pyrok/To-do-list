39c051e0b163381fff35c2ced338621ca115e48f
Pyrok <ulysse.petit@gmail.com>
2017-09-20 10:31:42 +0200

    Update

--- todolist.py
+++ todolist.py
@@ -3,6 +3,7 @@
  
 from Tkinter import *
 from tkMessageBox import *
+import json
 
 framesList = []
 todoList = []
@@ -10,50 +11,43 @@ inProgressList = []
 doneList = []
 tasksList = []
 
-def move(taskButton):
-    taskName = taskButton['text']
-    taskButton.pack_forget()
-    if taskName in tasksList[0]:
-        #print("Item in the first list")
-        taskButton.pack_forget()
-        tasksList[0].remove(taskName)
-        tasksList[1].append(taskName)
-        newButton = Button(framesList[3], text=taskName, command=lambda: move(newButton))
-        newButton["bg"]="orange"
-        newButton["overrelief"]="ridge"
-        newButton["wraplength"]=150
-        newButton.pack(fill=BOTH, padx=5, pady=5)
-        return
-    if taskName in tasksList[1]:
-        #print("Item in the second list")
-        taskButton.pack_forget()
-        tasksList[1].remove(taskName)
-        tasksList[2].append(taskName)
-        newButton = Button(framesList[4], text=taskName, command=lambda: move(newButton))
-        newButton["bg"]="#04B404"
-        newButton["overrelief"]="ridge"
-        newButton["wraplength"]=150
-        newButton.pack(fill=BOTH, padx=5, pady=5)
-        return
-    if taskName in tasksList[2]:
-        #print("Item in the third list")
-        tasksList[2].remove(taskName)
-        taskButton.pack_forget()
-        return
+def save():
+    savedList = json.dumps(tasksList)
+    file = open("savedTasks.txt","w")
+    file.write(savedList)
+    file.close()
 
+def load():
+    global tasksList
+    file = open("savedTasks.txt","r")
+    savedList = json.loads(file.read())
+    tasksList = savedList
+    for item in tasksList[0]:
+        addTask(item, 2)
+    for item in tasksList[1]:
+        addTask(item, 3)
+    for item in tasksList[2]:
+        addTask(item, 4)
+    file.close()
+	
+def test():
+    print(tasksList)
+	
 def createFrames(master):
     menubar = Menu(master)
     #Menu 1
     menu1 = Menu(menubar, tearoff=0)
     menu1.add_command(label="New list")
     menu1.add_command(label="New task", command=lambda: newTaskFrameUpdate(master))
-    menu1.add_command(label="Load")
+    menu1.add_command(label="Load", command=load)
+    menu1.add_command(label="Save", command=save)
     menu1.add_separator()
     menu1.add_command(label="Quit", command=master.quit)
     menubar.add_cascade(label="File", menu=menu1)
     #Menu 3
     menu2 = Menu(menubar, tearoff=0)
     menu2.add_command(label="About", command=lambda:showwarning("About", "Developed by Ulysse PETIT"))
+    menu2.add_command(label="Test", command=test)
     menubar.add_cascade(label="Help", menu=menu2)
 
     master.config(menu=menubar)
@@ -97,21 +91,58 @@ def newTaskFrameUpdate(master):
     labelName.pack(side=TOP, padx=5, pady=5)
     entryField = Entry(framesList[0])
     entryField.pack(fill=X, expand=True, padx=5, pady=5)
-    buttonAdd = Button(framesList[0], text="Add", command=lambda: addTask(master, labelName, entryField, buttonAdd))
+    buttonAdd = Button(framesList[0], text="Add", command=lambda: addTaskManually(master, labelName, entryField, buttonAdd))
     buttonAdd.pack(fill=X, padx=5, pady=5)	
 
-def addTask(master, label, entry, button):
+def addTaskManually(master, label, entry, button):
+    global tasksList
     taskName = entry.get()
     tasksList[0].append(taskName)
-    newButton = Button(framesList[2], text=taskName, command=lambda: move(newButton))
-    newButton["bg"]="#FA5858"
-    newButton["overrelief"]="ridge"
-    newButton["wraplength"]=150
-    newButton.pack(fill=BOTH, padx=5, pady=5)
+    addTask(taskName, 2)
     label.pack_forget()
     entry.pack_forget()
     button.pack_forget()
     framesList[0].pack_forget()
+
+def addTask(buttonName, frameId):
+    newTask = Button(framesList[frameId], text=buttonName, command=lambda: move(newTask))
+    if frameId == 2:
+        newTask["bg"]="#FA5858"
+    if frameId == 3:
+        newTask["bg"]="orange"
+    if frameId == 4:
+        newTask["bg"]="#04B404"
+    newTask["overrelief"]="ridge"
+    newTask["wraplength"]=150
+    newTask.pack(fill=BOTH, padx=5, pady=5)
+
+def move(taskButton):
+    global tasksList
+    taskName = taskButton['text']
+    if taskName in tasksList[0]:
+        taskButton.pack_forget()
+        tasksList[0].remove(taskName)
+        tasksList[1].append(taskName)
+        newButton = Button(framesList[3], text=taskName, command=lambda: move(newButton))
+        newButton["bg"]="orange"
+        newButton["overrelief"]="ridge"
+        newButton["wraplength"]=150
+        newButton.pack(fill=BOTH, padx=5, pady=5)
+        return
+    if taskName in tasksList[1]:
+        taskButton.pack_forget()
+        tasksList[1].remove(taskName)
+        tasksList[2].append(taskName)
+        newButton = Button(framesList[4], text=taskName, command=lambda: move(newButton))
+        newButton["bg"]="#04B404"
+        newButton["overrelief"]="ridge"
+        newButton["wraplength"]=150
+        newButton.pack(fill=BOTH, padx=5, pady=5)
+        return
+    if taskName in tasksList[2]:
+        taskButton.pack_forget()
+        tasksList[2].remove(taskName)
+        return
 
 def main():
     #Initialising the lists
@@ -122,6 +153,7 @@ def main():
     master = Tk()
     master["bg"]="white"
     master.title("To do list - by Ulysse PETIT")
+    master.resizable(width=FALSE, height=FALSE)
     createFrames(master)
     master.mainloop()
 
